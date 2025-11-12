1544ac66561d5f865bf67a1db9a87d9dcc59d86d
shanmugaraja M <shanmugarajaofficial@gmail.com>
2025-11-12 11:18:07 +0530

    destroy instance

--- main.tf
+++ main.tf
@@ -3,11 +3,4 @@ provider "aws" {
    secret_key = ""
 }
 
-resource "aws_instance" "Dev" {
-  ami = "ami-03695d52f0d883f65"
-  instance_type = "t2.micro"
-  key_name = "webserver"
-  tags = {
-    Name = "terraform_cloud_instance"
-  }
-}
+
